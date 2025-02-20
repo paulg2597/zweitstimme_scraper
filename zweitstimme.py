18ee1096ea867ea5d8f91b27b3495dca99c11f66
paulg2597 <128136658+paulg2597@users.noreply.github.com>
2025-02-20 13:14:01 +0100

    Initial Commit

---
+++ zweitstimme.py
@@ -0,0 +1,13 @@
+import requests as re
+import json
+from datetime import datetime
+
+zweit_resp = re.get("https://polsci.uni-wh.de/forecast", verify=False)
+zweit_content = json.dumps(json.loads(zweit_resp.content))
+
+erst_resp = re.get("https://polsci.uni-wh.de/forecast_districts", verify=False)
+erst_content = json.dumps(json.loads(erst_resp.content))
+
+for file, name in zip([zweit_content, erst_content], ["zweit_content", "erst_content"]):
+    with open(f'data/{name}_{datetime.now().isoformat()[:-10]}.json', 'w') as f:
+        f.write(file)
