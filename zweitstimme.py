0f31f1de579727c5ff7b82297eebdf5557f3b837
paulg2597 <128136658+paulg2597@users.noreply.github.com>
2025-02-20 13:31:24 +0100

    Bot integration

--- zweitstimme.py
+++ zweitstimme.py
@@ -1,13 +1,21 @@
 import requests as re
 import json
 from datetime import datetime
+import telebot
+import os
 
-zweit_resp = re.get("https://polsci.uni-wh.de/forecast", verify=False)
-zweit_content = json.dumps(json.loads(zweit_resp.content))
+os.chdir(os.getcwd())
 
-erst_resp = re.get("https://polsci.uni-wh.de/forecast_districts", verify=False)
-erst_content = json.dumps(json.loads(erst_resp.content))
+try:
+    zweit_resp = re.get("https://polsci.uni-wh.de/forecast", verify=False)
+    zweit_content = json.dumps(json.loads(zweit_resp.content))
 
-for file, name in zip([zweit_content, erst_content], ["zweit_content", "erst_content"]):
-    with open(f'data/{name}_{datetime.now().isoformat()[:-10]}.json', 'w') as f:
-        f.write(file)
+    erst_resp = re.get("https://polsci.uni-wh.de/forecast_districts", verify=False)
+    erst_content = json.dumps(json.loads(erst_resp.content))
+
+    for file, name in zip([zweit_content, erst_content], ["zweit_content", "erst_content"]):
+        with open(f'data/{name}_{datetime.now().isoformat()[:-10]}.json', 'w') as f:
+            f.write(file)
+except Exception as e:
+    bot = telebot.TeleBot(os.environ['token'])
+    bot.send_message(os.environ['chat_id'], f'Error! \n {e}')
