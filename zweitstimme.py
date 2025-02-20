18c391a0bbafec8c3be7b8fc89164e1e19bf872e
paulg2597 <128136658+paulg2597@users.noreply.github.com>
2025-02-20 13:52:51 +0100

    Bot integration

--- zweitstimme.py
+++ zweitstimme.py
@@ -4,8 +4,6 @@ from datetime import datetime
 import telebot
 import os
 
-os.chdir(os.getcwd())
-
 try:
     zweit_resp = re.get("https://polsci.uni-wh.de/forecast", verify=False)
     zweit_content = json.dumps(json.loads(zweit_resp.content))
@@ -14,7 +12,7 @@ try:
     erst_content = json.dumps(json.loads(erst_resp.content))
 
     for file, name in zip([zweit_content, erst_content], ["zweit_content", "erst_content"]):
-        with open(f'data/{name}_{datetime.now().isoformat()[:-10]}.json', 'w') as f:
+        with open(f'{os.getcwd()}/data/{name}_{datetime.now().isoformat()[:-10]}.json', 'w') as f:
             f.write(file)
 except Exception as e:
     bot = telebot.TeleBot(os.environ['token'])
